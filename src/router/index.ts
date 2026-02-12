c8d2af14d99267636b0f35a0e705816602d1a3af
preca-hoshino <001@labmem.com.cn>
2026-02-12 10:26:48 +0800

    [Fix] Ensure consistent trailing slashes in URL and navigation

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
 import { type RouteRecordRaw } from 'vue-router'
 
 export const navItems = [
-  { name: '项目', path: '/projects' },
-  { name: '应用', path: '/gadgets' },
-  { name: '成员', path: '/labmems' },
-  { name: '关于', path: '/about' },
+  { name: '项目', path: '/projects/' },
+  { name: '应用', path: '/gadgets/' },
+  { name: '成员', path: '/labmems/' },
+  { name: '关于', path: '/about/' },
 ]
 
 export const routes: RouteRecordRaw[] = [
@@ -15,22 +15,22 @@ export const routes: RouteRecordRaw[] = [
     component: () => import('@/views/MainPage.vue'),
   },
   {
-    path: '/projects',
+    path: '/projects/',
     name: 'projects',
     component: () => import('@/views/ProjectsPage.vue'),
   },
   {
-    path: '/gadgets',
+    path: '/gadgets/',
     name: 'gadgets',
     component: () => import('@/views/GadgetsPage.vue'),
   },
   {
-    path: '/labmems',
+    path: '/labmems/',
     name: 'members',
     component: () => import('@/views/LabmemPage.vue'),
   },
   {
-    path: '/about',
+    path: '/about/',
     name: 'about',
     component: () => import('@/views/AboutPage.vue'),
   },
